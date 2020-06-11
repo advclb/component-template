35d3b2366d93d8cff0b82bea0e76753bf7949eff
Guo Yunhe <i@guoyunhe.me>
2020-06-11 18:38:25 +0300

    doc(Slides): update example

--- packages/react/doc/src/pages/SlidesPage.tsx
+++ packages/react/doc/src/pages/SlidesPage.tsx
@@ -1,9 +1,29 @@
 import React, { ReactElement, useState } from "react";
 import { PropsDoc } from "../PropsDoc";
 import Container from "../Container";
+import Example from "../Example";
 import { Button } from "../../../src/Button";
 import { Slides } from "../../../src/Slides";
 import doc from "!!@advclb/react-docgen-loader!../../../src/Slides";
+
+function DummyCard({ children }: { children: string }): ReactElement {
+  return (
+    <div
+      style={{
+        background: "#555",
+        color: "#fff",
+        width: 300,
+        height: 200,
+        fontSize: 96,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {children}
+    </div>
+  );
+}
 
 export function SlidesPage(): ReactElement {
   const [activeIndex, setActiveIndex] = useState(0);
@@ -19,7 +39,32 @@ export function SlidesPage(): ReactElement {
         >
           -
         </Button>
-        {activeIndex}
+
+        <Button
+          onClick={(): void => setActiveIndex(0)}
+          disabled={activeIndex === 0}
+        >
+          1
+        </Button>
+        <Button
+          onClick={(): void => setActiveIndex(1)}
+          disabled={activeIndex === 1}
+        >
+          2
+        </Button>
+        <Button
+          onClick={(): void => setActiveIndex(2)}
+          disabled={activeIndex === 2}
+        >
+          3
+        </Button>
+        <Button
+          onClick={(): void => setActiveIndex(3)}
+          disabled={activeIndex === 3}
+        >
+          4
+        </Button>
+
         <Button
           onClick={(): void => setActiveIndex(activeIndex + 1)}
           disabled={activeIndex > 2}
@@ -27,12 +72,21 @@ export function SlidesPage(): ReactElement {
           +
         </Button>
       </p>
-      <Slides activeIndex={activeIndex} onActiveIndexChanged={setActiveIndex}>
-        <div style={{ background: "#ff8888", width: 400, height: 400 }}>1</div>
-        <div style={{ background: "#ff8888", width: 400, height: 400 }}>2</div>
-        <div style={{ background: "#ff8888", width: 400, height: 400 }}>3</div>
-        <div style={{ background: "#ff8888", width: 400, height: 400 }}>4</div>
-      </Slides>
+      <Example
+        jsx={`<Slides activeIndex={activeIndex} onActiveIndexChanged={setActiveIndex}>
+  <div>...</div>
+  <div>...</div>
+  <div>...</div>
+  <div>...</div>
+</Slides>`}
+      >
+        <Slides activeIndex={activeIndex} onActiveIndexChanged={setActiveIndex}>
+          <DummyCard>1</DummyCard>
+          <DummyCard>2</DummyCard>
+          <DummyCard>3</DummyCard>
+          <DummyCard>4</DummyCard>
+        </Slides>
+      </Example>
 
       <hr></hr>
 
